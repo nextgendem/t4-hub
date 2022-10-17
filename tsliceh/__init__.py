89a1d1e7a42012207aff95c8e68f91ad5f07a23d
Paula <paulavimf@gmail.com>
2022-10-17 17:06:46 +0100

    up docker compose when the app initiates

--- tsliceh/__init__.py
+++ tsliceh/__init__.py
@@ -2,15 +2,16 @@ import datetime
 import uuid
 
 import docker
+from python_on_whales import docker as docker_ow
 from docker.errors import APIError
 from sqlalchemy import Column, JSON, Boolean, String, DateTime, TypeDecorator, CHAR
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 
-
-from ldap3 import Server, Connection, ALL, SUBTREE
-from ldap3.core.exceptions import LDAPException, LDAPBindError
-
+# from ldap3 import Server, Connection, ALL, SUBTREE
+# from ldap3.core.exceptions import LDAPException, LDAPBindError
+#
+from tsliceh.helpers import containers_status
 
 
 class GUID(TypeDecorator):
@@ -89,43 +90,51 @@ def create_tables(engine_, declarative_base_=SQLAlchemyBase):
         declarative_base_.metadata.create_all()
 
 
-def connect_ldap_server():
+# def connect_ldap_server():
+#     try:
+#
+#         # Provide the hostname and port number of the openLDAP
+#         # TODO FIND ldap ip
+#         server_uri = f"ldap://192.168.1.3:389"
+#         server = Server(server_uri, get_info=ALL)
+#         # username and password can be configured during openldap setup
+#         connection = Connection(server,
+#                                 user='cn=admin,dc=testldap,dc=com',
+#                                 password=PASSWORD)
+#         bind_response = connection.bind()  # Returns True or False
+#     except LDAPBindError as e:
+#         connection = e
+#
+#
+# # For groups provide a groupid number instead of a uidNumber
+# def get_ldap_users():
+#     # Provide a search base to search for.
+#     search_base = 'dc=testldap,dc=com'
+#     # provide a uidNumber to search for. '*" to fetch all users/groups
+#     search_filter = '(uidNumber=500)'
+#
+#     # Establish connection to the server
+#     ldap_conn = connect_ldap_server()
+#     try:
+#         # only the attributes specified will be returned
+#         ldap_conn.search(search_base=search_base,
+#                          search_filter=search_filter,
+#                          search_scope=SUBTREE,
+#                          attributes=['cn', 'sn', 'uid', 'uidNumber'])
+#         # search will not return any values.
+#         # the entries method in connection object returns the results
+#         results = connection.entries
+#     except LDAPException as e:
+#         results = e
 
-    try:
 
-        # Provide the hostname and port number of the openLDAP
-        # TODO FIND ldap ip
-        server_uri = f"ldap://192.168.1.3:389"
-        server = Server(server_uri, get_info=ALL)
-        # username and password can be configured during openldap setup
-        connection = Connection(server,
-                                user='cn=admin,dc=testldap,dc=com',
-                                password=PASSWORD)
-        bind_response = connection.bind() # Returns True or False
-    except LDAPBindError as e:
-        connection = e
-
-# For groups provide a groupid number instead of a uidNumber
-def get_ldap_users():
-
-    # Provide a search base to search for.
-    search_base = 'dc=testldap,dc=com'
-    # provide a uidNumber to search for. '*" to fetch all users/groups
-    search_filter = '(uidNumber=500)'
-
-    # Establish connection to the server
-    ldap_conn = connect_ldap_server()
-    try:
-        # only the attributes specified will be returned
-        ldap_conn.search(search_base=search_base,
-                         search_filter=search_filter,
-                         search_scope=SUBTREE,
-                         attributes=['cn','sn','uid','uidNumber'])
-        # search will not return any values.
-        # the entries method in connection object returns the results
-        results = connection.entries
-    except LDAPException as e:
-        results = e
+def docker_compose_up():
+    compose = docker_ow.compose.up(detach=True)
+    for container in docker_ow.compose.ps():
+        status = containers_status(container.name)
+        print(f"{container.name} : {status}")
+        if status == "exited":
+            raise APIError(500, f"Error running {container.name} : status : {status} \n logs: {container.logs(tail=10)}")
 
 
 def create_docker_network(network_name):
@@ -138,18 +147,18 @@ def create_docker_network(network_name):
     """
     dc = docker.from_env()
     # print("networks inside container " + dc.networks.list(names = network_name))
-    networks_list = dc.networks.list(names = network_name)
-    if len(networks_list)>0:
-       if len(networks_list) > 1:
+    networks_list = dc.networks.list(names=network_name)
+    if len(networks_list) > 0:
+        if len(networks_list) > 1:
             for network in networks_list:
                 # check if there are containers attached to the network
                 if len(network.containers) == 0:
                     network.remove()
-                networks_list = dc.networks.list(names = network_name)
+                networks_list = dc.networks.list(names=network_name)
     if len(networks_list) == 1:
         return networks_list[0].id
     elif len(networks_list) == 0:
         network = dc.networks.create(network_name, driver="bridge")
         return network.id
     else:
-        raise APIError(500, details = f"There is more than one {network_name} network active")
+        raise APIError(500, details=f"There is more than one {network_name} network active")
